45c489bd7de5458fda5aac95cb13b086de6b2e02
cawhite7 <90364342+cawhite7@users.noreply.github.com>
2022-02-24 21:59:15 -0600

    Update DOC-Link.ps1

--- DOC-Link.ps1
+++ DOC-Link.ps1
@@ -28,19 +28,19 @@ if ($IoU -eq 'i') {
             DISM /Online /Enable-Feature /FeatureName:NetFx3 /Quiet /All
 
             $05 = Write-Host "Starting DOC-Link with Admin Installation....."
-            $075 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\Admin.txt"
+            $075 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\Admin.txt"
             $085 = Write-Host "`nProgess...`n[                (00.0%)               ]" -NoNewline
-            $SmartClient = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\1.exe" -ArgumentList "/passive","/v/qn" -Wait
+            $SmartClient = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\1.exe" -ArgumentList "/passive","/v/qn" -Wait
             $15 = Write-Host "`r[######          (16.7%)               ]" -NoNewline
-            $2 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\2\doc-link 3.0 Administrative Components.msi" /passive -wait
+            $2 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\2\doc-link 3.0 Administrative Components.msi" /passive -wait
             $25 = Write-Host "`r[############    (33.3%)               ]" -NoNewline
-            $3 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\3.exe" -ArgumentList "/passive","/v/qn" -Wait
+            $3 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\3.exe" -ArgumentList "/passive","/v/qn" -Wait
             $35 = Write-Host "`r[################(50.0%)               ]" -NoNewline
-            $4 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\4.msi" /passive -wait
+            $4 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\4.msi" /passive -wait
             $45 = Write-Host "`r[################(66.7%)###            ]" -NoNewline
-            $5 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\5.msi" /passive -wait
+            $5 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\5.msi" /passive -wait
             $55 = Write-Host "`r[################(83.3%)#########      ]" -NoNewline
-            $6 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\6\Microsoft Dynamics NAV RoleTailored Client2.msi" -wait
+            $6 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\6\Microsoft Dynamics NAV RoleTailored Client2.msi" -wait
             $65 = Write-Host "`r[###############(100.0%)###############]"
             $67 = Write-Host "DOC-Link FINISHED ..... Closing"
             $7 = $05,$075,$085,$SmartClient,$15,$2,$25,$3,$35,$4,$45,$5,$55,$6,$65,$67
@@ -81,17 +81,17 @@ if ($IoU -eq 'i') {
             DISM /Online /Enable-Feature /FeatureName:NetFx3 /Quiet /All
 
             $205 = Write-Host "Starting DOC-Link Installation....."
-            $2075 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\NotAdmin.txt"
+            $2075 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\NotAdmin.txt"
             $2085 = Write-Host "`nProgess...`n[                (00.0%)               ]" -NoNewline
-            $21 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\1.exe" -ArgumentList "/passive","/v/qn" -Wait
+            $21 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\1.exe" -ArgumentList "/passive","/v/qn" -Wait
             $215 = Write-Host "`r[########        (20.0%)               ]" -NoNewline
-            $23 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\3.exe" -ArgumentList "/passive","/v/qn" -Wait
+            $23 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\3.exe" -ArgumentList "/passive","/v/qn" -Wait
             $235 = Write-Host "`r[############### (40.0%)               ]" -NoNewline
-            $24 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\4.msi" /passive -wait
+            $24 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\4.msi" /passive -wait
             $245 = Write-Host "`r[################(60.0%)               ]" -NoNewline
-            $25 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\5.msi" /passive -wait
+            $25 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\5.msi" /passive -wait
             $255 = Write-Host "`r[################(80.0%)#######        ]" -NoNewline
-            $26 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\6\Microsoft Dynamics NAV RoleTailored Client2.msi" -wait
+            $26 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\6\Microsoft Dynamics NAV RoleTailored Client2.msi" -wait
             $265 = Write-Host "`r[###############(100.0%)###############]"
             $267 = Write-Host "DOC-Link FINISHED ..... Closing"
             $27 = $205,$2075,$2085,$21,$215,$22,$225,$23,$235,$24,$245,$25,$255,$26,$265,$267
@@ -144,17 +144,17 @@ elseif ($IoU -eq 'u') {
             if ($admin -eq 'y') {
             $05 = Write-Host "Starting DOC-Link with Admin Uninstallation....."
             $085 = Write-Host "`nProgess...`n[                (00.0%)               ]" -NoNewline
-            $SmartClient = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\1.exe" -Wait
+            $SmartClient = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\1.exe" -Wait
             $15 = Write-Host "`r[######          (16.7%)               ]" -NoNewline
-            $2 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\2\doc-link 3.0 Administrative Components.msi" -Wait
+            $2 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\2\doc-link 3.0 Administrative Components.msi" -Wait
             $25 = Write-Host "`r[############    (33.3%)               ]" -NoNewline
-            $3 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\3.exe" -Wait
+            $3 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\3.exe" -Wait
             $35 = Write-Host "`r[################(50.0%)               ]" -NoNewline
-            $4 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\4.msi" -Wait
+            $4 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\4.msi" -Wait
             $45 = Write-Host "`r[################(66.7%)###            ]" -NoNewline
-            $5 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\5.msi" -Wait
+            $5 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\5.msi" -Wait
             $55 = Write-Host "`r[################(83.3%)#########      ]" -NoNewline
-            $6 = msiexec.exe /x "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\6\Microsoft Dynamics NAV RoleTailored Client2.msi"
+            $6 = msiexec.exe /x "\\share\UserAccessApps\StoredApps\DOC-Link\6\Microsoft Dynamics NAV RoleTailored Client2.msi"
             $65 = Write-Host "`r[###############(100.0%)###############]"
             $67 = Write-Host "DOC-Link FINISHED ..... Closing"
             $7 = $05,$075,$085,$SmartClient,$15,$2,$25,$3,$35,$4,$45,$5,$55,$6,$65,$67
@@ -180,15 +180,15 @@ elseif ($IoU -eq 'u') {
             elseif ($admin -eq 'n') {
             $205 = Write-Host "Starting DOC-Link Uninstallation....."
             $2085 = Write-Host "`nProgess...`n[                (00.0%)               ]" -NoNewline
-            $21 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\1.exe" -Wait
+            $21 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\1.exe" -Wait
             $215 = Write-Host "`r[########        (20.0%)               ]" -NoNewline
-            $23 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\3.exe" -Wait
+            $23 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\3.exe" -Wait
             $235 = Write-Host "`r[############### (40.0%)               ]" -NoNewline
-            $24 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\4.msi" -Wait
+            $24 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\4.msi" -Wait
             $245 = Write-Host "`r[################(60.0%)               ]" -NoNewline
-            $25 = Start-Process "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\5.msi" -Wait
+            $25 = Start-Process "\\share\UserAccessApps\StoredApps\DOC-Link\5.msi" -Wait
             $255 = Write-Host "`r[################(80.0%)#######        ]" -NoNewline
-            $26 = msiexec.exe /x "\\cpn-deploy\UserAccessApps\StoredApps\DOC-Link\6\Microsoft Dynamics NAV RoleTailored Client2.msi"
+            $26 = msiexec.exe /x "\\share\UserAccessApps\StoredApps\DOC-Link\6\Microsoft Dynamics NAV RoleTailored Client2.msi"
             $265 = Write-Host "`r[###############(100.0%)###############]"
             $267 = Write-Host "DOC-Link FINISHED ..... Closing"
             $27 = $205,$2085,$21,$215,$22,$225,$23,$235,$24,$245,$25,$255,$26,$265,$267
